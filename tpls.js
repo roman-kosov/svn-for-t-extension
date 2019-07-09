6756ee85a07c3d4f6a1e1c83a8a01a93b6b2700f
roman-kosov <i-am-roman-kosov@ya.ru>
2019-07-10 00:08:50 +0300

    Отключаем логгирование

--- tpls.js
+++ tpls.js
@@ -159,7 +159,8 @@ if (document.body) {
 function shotBlock(recid) {
     html2canvas(document.querySelector("#rec" + recid), {
         allowTaint: true,
-        useCORS: true
+        useCORS: true,
+        logging: false
     }).then(canvas => {
         var a = document.createElement('a');
         a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
